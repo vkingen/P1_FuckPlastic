bea643666ec43e619c1acb60f95a1b67a1c98c1f
akaise21 <akaise21@student.aau.dk>
2021-11-23 13:47:25 +0100

    Tilføjet scripts
    GameBehavior_Aske og GameMenuUI_Aske

---
+++ P1_FUCKYOUPLASTIC/Assets/Scripts/GameMenuUI_Aske.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMenuUI_Aske : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnGUI()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
