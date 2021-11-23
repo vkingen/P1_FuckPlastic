ecca1c55d7036166760467e2a114c47e2f310710
akaise21 <akaise21@student.aau.dk>
2021-11-23 14:06:21 +0100

    Update GameBehavior_Aske.cs

--- P1_FUCKYOUPLASTIC/Assets/Scripts/GameBehavior_Aske.cs
+++ P1_FUCKYOUPLASTIC/Assets/Scripts/GameBehavior_Aske.cs
@@ -4,15 +4,65 @@ using UnityEngine;
 
 public class GameBehavior_Aske : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // 1
+    public string labelText = "Collect all 4 items and win your freedom!";
+    public int maxItems = 4;
+    private int _itemsCollected = 0;
+    public int Items
     {
-        
+        get { return _itemsCollected; }
+        set
+        {
+            _itemsCollected = value;
+
+            // 2
+            if (_itemsCollected >= maxItems)
+            {
+                labelText = "You've found all the items!";
+            }
+            else
+            {
+                labelText = "Item found, only " + (maxItems -
+                _itemsCollected) + " more to go!";
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private int _playerHP = 3;
+    public int HP
     {
-        
+        get { return _playerHP; }
+        set
+        {
+            _playerHP = value;
+            Debug.LogFormat("Lives: {0}", _playerHP);
+        }
+    }
+
+    // 3
+    void OnGUI()
+    {
+        // 4
+        GUI.Box(new Rect(20, 20, 150, 25), "Player Health:" +
+         _playerHP);
+        // 5
+        GUI.Box(new Rect(20, 50, 150, 25), "Items Collected: " +
+        _itemsCollected);
+        // 6
+        GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height -
+        50, 300, 50), labelText);
     }
 }
+
+// Start is called before the first frame update
+//void Start()
+//    {
+        
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+        
+//    }
+//}
