cd4b3339ba1e77a4689a3e03a7e2463316e78af1
lokal-freshboi <augustjuul55@gmail.com>
2021-11-24 15:10:54 +0100

    powerup

--- P1_FUCKYOUPLASTIC/Assets/Scripts/August/PUballsizePlus.cs
+++ P1_FUCKYOUPLASTIC/Assets/Scripts/August/PUballsizePlus.cs
@@ -3,27 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PUballsizePlus : MonoBehaviour
+
 {
-        public GameObject pickupEffect;
+    public float multiplier = 2.5f;
+
+    public GameObject pickupEffect;
+
+    public GameObject Bolder_Player1;
+    public GameObject Bolder_Player2;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"));
+        if (other.CompareTag("Player1"))
 
         {
-            Pickup();
-
+            Pickup(other);
         }
+
+        else if (other.CompareTag("Player2"))
+
+        {
+            Pickup2(other);
+        }
+
+
     }
 
 
-    void Pickup()
+    void Pickup(Collider Bolder_Player1)
     {
         Instantiate(pickupEffect, transform.position, transform.rotation);
 
+        Bolder_Player1 = GameObject.Find("Bolder_Player1");
 
-
+        Bolder_Player1.transform.localScale *= multiplier;
 
         Destroy(gameObject);
+
+    }
+    void Pickup2(Collider Bolder_Player2)
+    {
+        Instantiate(pickupEffect, transform.position, transform.rotation);
+
+        Bolder_Player2 = GameObject.Find("Bolder_Player2");
+
+        Bolder_Player2.transform.localScale *= multiplier;
+
+        Destroy(gameObject);
+
     }
 }
