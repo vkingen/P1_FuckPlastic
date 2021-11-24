4deef1029b5cce8f61d8aade391736fc6bbd7584
vkingen <lassevikingkristensen@gmail.com>
2021-11-24 16:39:35 +0100

    5

--- P1_FUCKYOUPLASTIC/Assets/ScreamOnTrigger.cs
+++ P1_FUCKYOUPLASTIC/Assets/ScreamOnTrigger.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class ScreamOnTrigger : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other) {
-        if (other.tag == "Player" && other.tag == "Player2")
-        {
+    AudioSource aS;
 
+    private void Start()
+    {
+        aS = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Player" || other.gameObject.tag == "Player2")
+        {
+            aS.Play();
+            Debug.Log("PLAY AUDIO");
         }
+        Debug.Log("swagger");
     }
 }
